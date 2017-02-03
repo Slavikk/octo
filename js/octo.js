db471f2a33498f38f2bc199dd0ae6920955ed3ac
slava.home <fareastaz@gmail.com>
2017-02-03 01:31:33 -0500

    Adding search block

--- js/octo.js
+++ js/octo.js
@@ -1,7 +1,24 @@
 ! function($) {
     $(document).ready(function() {
 
+        //-- Search icon
+        (function() {
 
+            $(".open-form").click(function(){
+                $(".open-form").hide();
+                $(".close-form").css("display","block");
+                $(".search-block-form").show();
+                $(".search-block-form input").focus();
+                return false;
+            });
+            $(".close-form").click(function(){
+                $(".close-form").hide();
+                $(".open-form").css("display","block");
+                $(".search-block-form").hide();
+                return false;
+            });
+
+        })();
 
     });
 }(jQuery);
